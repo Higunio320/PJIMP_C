411ade0535a8902a2c8e5c3f26ea9d79c14f0bcc
Higunio320 <wojtek123713@gmail.com>
2022-03-13 12:55:58 +0100

    1. wersja algorytmu BFS

---
+++ bfs.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bfs.h"
+
+void double_size(int* a, int* n) {
+	a = realloc(a, 2*(*n)*sizeof(a));
+	(*n) *= 2;
+}
+
+int bfs(wierzcholek** a, int rozmiar) {
+	int i;
+	int x = 1;
+	int n = 2;
+	int p = 0;
+	int tmp = 0;
+	int *kolejka;
+	kolejka = (int *) malloc (sizeof(int)*rozmiar);
+	for(i = 0; i < a[0]->n; i++) {
+		kolejka[i] = a[0]->w[i];
+		a[a[0]->w[i]]->stan = 1;
+		x++;
+		p++;
+	}
+	a[0]->stan = 1;
+	while(x != rozmiar) {
+		if(tmp == p) {
+			free(kolejka);
+			return 1;
+		}
+		tmp = p-1;
+		for(i = 0; i < a[kolejka[0]]->n; i++) {
+			/*if(p == n) 
+				double_size(kolejka, &n);*/
+			if(a[a[kolejka[0]]->w[i]]->stan == 0) {
+				kolejka[p] = a[kolejka[0]]->w[i];
+				a[a[kolejka[0]]->w[i]]->stan = 1;
+				x++;
+				p++;
+			}
+		}
+		kolejka[0] = kolejka[p-1];
+		p--;
+	}
+	free(kolejka);
+	return 0;
+}
+
