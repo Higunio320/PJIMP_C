f6a352b1da09e43b0f904c44050a06d5c100a41b
zarembw1 <wiktor.zaremba.stud@pw.edu.pl>
2022-03-31 13:37:51 +0200

    Dodanie komentarzy

--- writer.c
+++ writer.c
@@ -5,22 +5,22 @@
 int zapisz_graf(char* file_name, wierzcholek** wierzcholek, int wysokosc, int szerokosc) {
 
 	int i,j;
-	FILE *out = fopen(file_name, "w"); // otwieram plik
+	FILE *out = fopen(file_name, "w"); /*otwieram plik*/
 
 	if(out == NULL) {
 		fprintf(out, "Nie moge otworzyc pliku!");
 		return 5;
-	} else { 
+	} 
+	else { 
 
-	fprintf(out, "%d %d\n", wysokosc, szerokosc); // wypiosuje kulumne i wiersz
-	for(i = 0; i < wysokosc * szerokosc; i++) { // wypisuje tyle wierzcholkow ile rozmiar grafu
+	fprintf(out, "%d %d\n", wysokosc, szerokosc); /*wypiosuje kulumne i wiersz*/
+	for(i = 0; i < wysokosc * szerokosc; i++) { /*wypisuje tyle wierzcholkow ile rozmiar grafu*/
 		for(j = 0; j < wierzcholek[i]->n; j++) {
 			fprintf(out, "\t%d :%f", wierzcholek[i]->w[j], wierzcholek[i]->drogi[j]);
-		}
+			}
 		fprintf(out, "\n");
 	}
 	fclose(out);
 	}
+	return 0;
 }
-		
-
